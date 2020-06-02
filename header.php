80463367cf3d9161724363cfb4b5b83715081398
Céline Pawlak <celine.pawlak@laplateforme.io>
2020-06-02 10:41:44 +0200

    php files created
    Inscription php done
    Delete session php done
    Header php done

---
+++ header.php
@@ -0,0 +1,80 @@
+<?php
+session_start();
+
+$db = mysqli_connect("localhost","root","","discussion");
+
+/*redirections de sessions*/
+
+if ($page_selected == "profil" AND !$_SESSION['id'])
+{
+  header('location: connexion.php')
+}
+if (in_array($page_selected, ['connexion','inscription']) AND isset($_SESSION['id']))
+{
+  header('location: index.php');
+}
+
+/*fonction erreurs*/
+
+/**
+  * @param $errors
+  * @return string
+  */
+function renderErrors($errors)
+{
+  if (!empty($errors))
+  {
+    $output = "";
+    if (count($errors) > 1)
+    {
+      $output .= "<ul>";
+      foreach ($errors as $error)
+      {
+        $output .= "<li>" . $error . "</li>";
+      }
+      $output .= "</ul>";
+    }
+    else
+    {
+      $output = $errors[0];
+    }
+    return "<div class=\"ErrorMessage\">"
+      . $output .
+      "</div>";
+  }
+  else
+  {
+    return "";
+  }
+}
+ ?>
+
+<!-- header html -->
+
+<nav>
+  <div class="header_1">
+    <a href="index.php"><h1>Home</h1></a>
+  </div>
+  <div class="header_2">
+    <a href="discussion.php"><h1>Chat</h1></a>
+  </div>
+  <div class="header_3">
+    <?php if (isset $_SESSION['id'])
+    { ?>
+      <ul>
+        <li class="liste">
+          <h1>My account</h1>
+          <ul class="sous_liste">
+            <li><a href="profil.php">Modify login and/or password</a></li>
+            <li><a href="delete_session.php">Disconnect</a></li>
+          </ul>
+        </li>
+      </ul>
+    <?php }
+    else { ?>
+        <a href="connexion.php"><h1>Connect</h1></a>
+        <p>or</p>
+        <a href="inscription.php"><h1>Register</h1></a>
+    <?php } ?>
+  </div>
+</nav>
---
+++ inscription.php
@@ -0,0 +1,84 @@
+<?php
+$page_selected = 'inscription';
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="fr" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title>Register - Discussion</title>
+     <link rel="stylesheet" href="styles/css/discussion.css">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+   <body>
+     <header>
+       <?php include 'header.php';
+       $errors = [];
+       if (!empty($_POST['login']) AND !empty($_POST['password']) AND !empty($_POST['conf_password']))
+       {
+         /*LOGIN*/
+         $login = $_POST['login'];
+         $login_required = preg_match("/^(?=.*[A-Za-z0-9]$)[A-Za-z][A-Za-z\d\-\_]{3,19}$/", $login);
+         if (!$login_required)
+         {
+           $errors[] = "Login must :<br>- Contain between 4 and 20 characters.<br>- Start with a letter.<br>- End with a letter or a number.<br>- Not contain any special characters (except - and _).";
+         }
+         $request = "SELECT login FROM utilisateurs WHERE login  = '" . $login . "';";
+         $query = mysqli_query($db, $request);
+         $login_check = mysqli_fetch_array($query);
+         if (!empty($login_check))
+         {
+           $errors[] = "This user already exists.";
+         }
+
+         /*PASSWORD*/
+         $password_init = $_POST['password'];
+         $password_conf = $_POST['conf_password'];
+         $password_required = match_preg("/^(?=.*?[A-Z]{1,})(?=.*?[a-z]{1,})(?=.*?[0-9]{1,})(?=.*?[\W]{1,}).{8,20}$/", $password_init);
+         if (!$password_required)
+         {
+           $errors[] = "Password must :<br>- contain between 8 and 20 characters<br>- contain at least : 1 special character, 1 number, 1 upper and 1 lower case."
+         }
+
+         /*ENVOI BDD*/
+         if (empty($errors))
+         {
+           $password_modified = password_hash($password_init, PASSWORD_BCRYPT, array('cost' => 10));
+           $request = "INSERT INTO utilisateurs(login, password) VALUES ('" . $login . "', '" . $password_modified . "');";
+           $query = mysqli_query($db, $request);
+           header('location: connexion.php');
+         }
+       }
+       elseif (empty($_POST))
+       {
+         $errors[] = "Every field must be filled.";
+       }
+
+        ?>
+     </header>
+     <main>
+       <div class="content">
+         <?= renderErrors($errors) ?>
+         <h2>Resgiter</h2>
+         <form class="" action="inscription.php" method="post">
+           <div class="form_element">
+             <label for="login">Login</label>
+             <input type="text" name="login" value="" placeholder="login">
+           </div>
+           <div class="form_element">
+             <label for="password">Password</label>
+             <input type="text" name="password" value="" placeholder="password">
+           </div>
+           <div class="form_element">
+             <label for="password_conf">Confirm password</label>
+             <input type="text" name="password_conf" value="" placeholder="confirm password">
+           </div>
+           <button type="submit" name="register_submit">Register</button>
+         </form>
+       </div>
+     </main>
+     <footer>
+       <!-- A FAIRE -->
+     </footer>
+   </body>
+ </html>
